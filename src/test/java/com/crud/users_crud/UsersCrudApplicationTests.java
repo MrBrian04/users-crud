5cf2fb817ada4bd43399f550d6eea9a4b97cbd91
MrBrian <bvergelgalvan@gmail.com>
2026-05-01 15:16:25 -0500

    v1: Creacion del proyecto y Edicion del archivo application.yml

---
+++ src/test/java/com/crud/users_crud/UsersCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.users_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
